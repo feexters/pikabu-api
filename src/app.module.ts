eaaa469d92bf4290c113d56b818622f915da7c38
Danil Nepomnyashih <danil.nepomniashchikh@purrweb.com>
2022-03-19 17:15:44 +0600

    feat: add user entitty

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './common/services/typeorm-config.service';
+import { UsersModule } from './users/users.module';
+import * as configuration from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration.configuration],
+      validationSchema: configuration.validationSchema,
+      validationOptions: configuration.validationOptions,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
